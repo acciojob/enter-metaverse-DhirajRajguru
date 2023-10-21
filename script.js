00bd150c68cc36452c30978f1ebf2fefcd4c24bd
DhirajRajguru <110520184+DhirajRajguru@users.noreply.github.com>
2023-10-21 22:04:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const enterBtn = document.getElementById("enterBtn");
+const para  = document.getElementById("status");
+
+enterBtn.addEventListener('click',()=>{
+
+	para.innerText = "";
+
+	// para.innerText = "Entered Metaverse";
+	// para.style.fontSize = "50px";
+
+	
+	const h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+
+	para.appendChild(h1);
+	
+})
